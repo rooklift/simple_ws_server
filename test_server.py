7fdd26e5756007ae1ee191faf792eddc0f82e7fc
rooklift <16438795+rooklift@users.noreply.github.com>
2024-07-02 08:35:56 +0100

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -6,36 +6,39 @@ clients = []
 clients_lock = threading.Lock()
 
 
-def we_send_messages():
-    n = 0
-    while 1:
-        n += 1
-        with clients_lock:
-            for c in clients:
-                try:
-                    c.sendMessage(str(n))
-                except:
-                    pass
-        time.sleep(1)
+class Connection(swss.WebSocket):
+
+	def handleConnected(self):
+		with clients_lock:
+			clients.append(self)
+			print("Connected: {}".format(self.address))
+
+	def handleClose(self):
+		with clients_lock:
+			clients.remove(self)
+			print("Disconnected: {}".format(self.address))
+
+	def handleMessage(self):
+		print(self.data)
+
 
 def server_start():
-    server = swss.SimpleWebSocketServer('localhost', 8001, MyWebsocketExample, selectInterval = 0.1)
-    server.serveforever()
-
-class MyWebsocketExample(swss.WebSocket):
-
-    def handleConnected(self):
-        with clients_lock:
-            clients.append(self)
-
-    def handleClose(self):
-        with clients_lock:
-            clients.remove(self)
-
-    def handleMessage(self):
-        print(self.data)
+	server = swss.SimpleWebSocketServer('localhost', 8001, Connection, selectInterval = 0.1)
+	server.serveforever()
 
 
-threading.Thread(target = we_send_messages).start()
+def message_sender():
+	n = 0
+	while 1:
+		n += 1
+		with clients_lock:
+			for c in clients:
+				try:
+					c.sendMessage(str(n))
+				except:
+					pass
+		time.sleep(1)
+
+
 threading.Thread(target = server_start).start()
-
+threading.Thread(target = message_sender).start()
